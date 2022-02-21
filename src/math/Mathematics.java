0d162743a71dc15165c5144889ad882a24a6ab2e
matan <matan33214@gmail.com>
2022-02-21 14:58:26 +0200

    v2 and 3

--- src/math/Mathematics.java
+++ src/math/Mathematics.java
@@ -1,6 +1,7 @@
 package math;
 
 public class Mathematics {
+    private Mathematics(){}
     // Smallest decimal value (negative)
     public static final float DECIMAL_SMALLEST = Float.MIN_VALUE;
 
@@ -15,4 +16,46 @@ public class Mathematics {
 
     // 2*Pi constant
     public static final float PI_TIMES_2 = 6.28318530f;
+
+    // Function to test if two real numbers are (almost) equal
+    // We test if two numbers a and b are such that (a-b) are in [-EPSILON; EPSILON]
+    public static boolean ApproxEqual(float a, float b, float epsilon) {
+        float difference = a - b;
+        return (difference < epsilon && difference > -epsilon);
+    }
+
+    public static float ArcCos(float radians) {
+        return (float) StrictMath.acos(radians);
+    }
+
+    public static float ArcSin(float radians) {
+        return (float) StrictMath.asin(radians);
+    }
+
+    public static float ArcTan2(float a, float b) {
+        return (float) StrictMath.atan2(a, b);
+    }
+
+    // Function that returns the result of the "value" clamped by
+    // two others values "lowerLimit" and "upperLimit"
+    public static float Clamp(float value, float lowerLimit, float upperLimit) {
+        assert (lowerLimit <= upperLimit);
+        return Math.min(Math.max(value, lowerLimit), upperLimit);
+    }
+
+    public static float Cos(float radians) {
+        return (float) StrictMath.cos(radians);
+    }
+
+    public static float Sin(float radians) {
+        return (float) StrictMath.sin(radians);
+    }
+
+    public static float Sqrt(float a) {
+        return (float) StrictMath.sqrt(a);
+    }
+
+    public static float Tan(float radians) {
+        return (float) StrictMath.tan(radians);
+    }
 }
