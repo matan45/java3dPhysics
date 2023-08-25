00cf38290fd517ca1422a53aec3f45e66ab1af90
matan migdal <matan33214@gmail.com>
2023-08-25 21:12:46 +0300

    terrain

---
+++ src/main/java/collisionDetection/primitive/TerrainShape.java
@@ -0,0 +1,10 @@
+package collisionDetection.primitive;
+
+public class TerrainShape {
+
+    private float[][] highData;//TODO splice for quad tree
+    private AABB borders;//define the min and max high
+    private static final float SIZE = 1024;
+    private int width;//define the width of the terrain
+    private int length;//define the length of the terrain
+}
