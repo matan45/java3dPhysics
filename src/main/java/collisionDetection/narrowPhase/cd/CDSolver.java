9cc2d3eae080f03d024e09b4b3434e0844b0e26c
matan migdal <matan33214@gmail.com>
2023-09-11 16:05:32 +0300

    solver cd

--- src/main/java/collisionDetection/narrowPhase/cd/CDSolver.java
+++ src/main/java/collisionDetection/narrowPhase/cd/CDSolver.java
@@ -4,6 +4,7 @@ import collisionDetection.narrowPhase.collisionResult.CollisionResult;
 import collisionDetection.primitive.Capsule;
 import collisionDetection.primitive.Cylinder;
 import collisionDetection.primitive.Line;
+import collisionDetection.primitive.Sphere;
 import math.Vector3f;
 
 import java.util.ArrayList;
@@ -11,7 +12,6 @@ import java.util.List;
 
 public class CDSolver {
     public static CollisionResult solve(Line line, Capsule capsule) {
-        CollisionResult result = new CollisionResult();
 
         // First, calculate the closest point on the line to the capsule's axis.
         Vector3f axis = capsule.getEnd().sub(capsule.getStart());
@@ -31,27 +31,18 @@ public class CDSolver {
         // Calculate the depth of penetration as the difference between the capsule's radius and the distance between the two closest points.
         float depth = capsule.getRadius() - closestPointOnLine.distance(closestPointOnAxis);
 
-        // Check if the depth is positive, indicating a collision.
-        if (depth > 0) {
-            result.setColliding(true);
-            result.setNormal(collisionNormal);
-            result.setDepth(depth);
-            List<Vector3f> contacts = new ArrayList<>();
-            // Calculate the contact points (one on each side of the capsule's axis).
-            Vector3f contactPoint1 = closestPointOnAxis.add(collisionNormal.mul(capsule.getRadius()));
-            Vector3f contactPoint2 = closestPointOnAxis.sub(collisionNormal.mul(capsule.getRadius()));
-            contacts.add(contactPoint1);
-            contacts.add(contactPoint2);
-            result.setContacts(contacts);
-        }
+        List<Vector3f> contacts = new ArrayList<>();
+        // Calculate the contact points (one on each side of the capsule's axis).
+        Vector3f contactPoint1 = closestPointOnAxis.add(collisionNormal.mul(capsule.getRadius()));
+        Vector3f contactPoint2 = closestPointOnAxis.sub(collisionNormal.mul(capsule.getRadius()));
+        contacts.add(contactPoint1);
+        contacts.add(contactPoint2);
 
-        return result;
+        return new CollisionResult(true, collisionNormal, depth, contacts);
     }
 
 
     public static CollisionResult solve(Line line, Cylinder cylinder) {
-        // Initialize the CollisionResult with default values
-        CollisionResult result = new CollisionResult();
 
         // Calculate the direction vector of the line segment
         Vector3f lineDirection = line.getEnd().sub(line.getStart());
@@ -68,25 +59,46 @@ public class CDSolver {
         // Calculate the closest point on the line segment to the cylinder's center
         Vector3f closestPointOnLine = line.getStart().add(lineDirection.mul(t));
 
-        // Check if the closest point on the line is inside the cylinder's bounds
-        if (cylinder.isPointInside(closestPointOnLine)) {
-            // Calculate the collision normal (from cylinder center to the closest point on the line)
-            Vector3f collisionNormal = closestPointOnLine.sub(cylinder.getCenter()).normalize();
+        // Calculate the collision normal (from cylinder center to the closest point on the line)
+        Vector3f collisionNormal = closestPointOnLine.sub(cylinder.getCenter()).normalize();
 
-            // Calculate the penetration depth
-            float penetrationDepth = cylinder.getRadius() - closestPointOnLine.distance(cylinder.getCenter());
+        // Calculate the penetration depth
+        float penetrationDepth = cylinder.getRadius() - closestPointOnLine.distance(cylinder.getCenter());
 
-            // Populate the CollisionResult
-            result.setColliding(true);
-            result.setNormal(collisionNormal);
-            result.setDepth(penetrationDepth);
-            result.getContacts().add(closestPointOnLine);
+        // Calculate and add the second contact point (reflection point)
+        Vector3f reflectionPoint = closestPointOnLine.add(collisionNormal.mul(penetrationDepth));
+        List<Vector3f> contacts = new ArrayList<>();
+        contacts.add(closestPointOnLine);
+        contacts.add(reflectionPoint);
 
-            return result;
-        }
+        return new CollisionResult(true, collisionNormal, penetrationDepth, contacts);
 
-        // If no collision, return the default CollisionResult indicating no collision
-        return result;
     }
+
+    public static CollisionResult solve(Line line, Sphere sphere) {
+
+        // The line's start point is inside the sphere.
+        // Calculate the closest point on the line to the sphere's center.
+        Vector3f closestPointOnLine = line.closestPoint(sphere.getCenter());
+
+        // Calculate the vector from the sphere's center to the closest point on the line.
+        Vector3f toClosestPointOnLine = closestPointOnLine.sub(sphere.getCenter());
+
+        // Calculate the collision normal (from the sphere's center to the closest point on the line).
+        Vector3f collisionNormal = toClosestPointOnLine.normalize();
+
+        // Calculate the collision depth (distance from the sphere's surface to the line).
+        float penetrationDepth = sphere.getRadius() - toClosestPointOnLine.length();
+
+        // Calculate the collision contact points.
+        Vector3f collisionContact = line.closestPoint(sphere.getCenter());
+
+        List<Vector3f> contacts = new ArrayList<>();
+        contacts.add(closestPointOnLine);
+        contacts.add(collisionContact);
+
+        return new CollisionResult(true, collisionNormal, penetrationDepth, contacts);
+    }
+
 
 }
