1c52a9239ece6df93260c402936dfc3e55244756
matan migdal <matan33214@gmail.com>
2023-09-20 09:50:44 +0300

    Particle

---
+++ src/main/java/physics/particle/ParticleForceRegistration.java
@@ -0,0 +1,4 @@
+package physics.particle;
+
+public record ParticleForceRegistration(Particle particle, ParticleForceGenerator fg) {
+}
--- src/main/java/physics/particle/ParticleForceRegistry.java
+++ src/main/java/physics/particle/ParticleForceRegistry.java
@@ -1,4 +1,33 @@
 package physics.particle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParticleForceRegistry {
+
+    private final List<ParticleForceRegistration> particleForceRegistrations;
+
+    public ParticleForceRegistry() {
+        particleForceRegistrations = new ArrayList<>();
+    }
+
+    public void add(ParticleForceRegistration particleForceRegistration) {
+        particleForceRegistrations.add(particleForceRegistration);
+    }
+
+    public void remove(ParticleForceRegistration particleForceRegistration) {
+        particleForceRegistrations.remove(particleForceRegistration);
+    }
+
+
+    public void clear() {
+        particleForceRegistrations.clear();
+    }
+
+
+    public void updateForces(float duration) {
+        for (ParticleForceRegistration particleForceRegistration : particleForceRegistrations) {
+            particleForceRegistration.fg().updateForce(particleForceRegistration.particle(), duration);
+        }
+    }
 }
